5423b7e5c7cfe0f050a6b68a1dc0507eb1e464d1
HopLee <39694692+HopLee6@users.noreply.github.com>
2020-05-16 13:58:59 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -32,12 +32,20 @@ def main():
         img1 = Image.open(im1_path)
         img2 = Image.open(im2_path)
 
-        img1 = transform(img1)
-        img2 = transform(img2)
-
-        img1, img2 = img1.cuda(), img2.cuda()
-        output = model(img1.unsqueeze(0), img2.unsqueeze(0))
-        output = output.squeeze(0).cpu()
+        img1 = transform(img1).unsqueeze(0).cuda()
+        img2 = transform(img2).unsqueeze(0).cuda()
+        
+        if img1.size(1)==1:
+            img1 = img1.expand(-1, 3,-1,-1)
+            img2 = img2.expand(-1, 3,-1,-1)
+            
+        _,_,H,W = img1.size()
+        H_,W_ = int(np.ceil(H/32)*32),int(np.ceil(W/32)*32)
+        pader = torch.nn.ReplicationPad2d([0, W_-W , 0, H_-H])
+        img1,img2 = pader(img1),pader(img2)
+        
+        output = model(img1, img2)
+        output = output[0,:,0:H,0:W].squeeze(0).cpu()
         output = transforms.functional.to_pil_image(output)
         output.save('data/im_interp.png')
 
